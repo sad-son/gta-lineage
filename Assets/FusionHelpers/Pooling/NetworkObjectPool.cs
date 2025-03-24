7eaeb2636631c1371e9e44a5d9b97d8ced5e63d4
Ayaz Sadykov <ayaz.sadykov@mytona.com>
2025-03-24 09:13:57 +0500

    removed unnecessary

--- Assets/FusionHelpers/Pooling/NetworkObjectPool.cs
+++
@@ -1,9 +0,0 @@
-using Fusion;
-
-namespace FusionHelpers
-{
-	public class NetworkObjectPool : ObjectPool<NetworkObject>
-	{
-
-	}
-}
--- Assets/Scripts/Tanknarok/Player/Effects/TargetMarker.cs
+++
@@ -1,7 +0,0 @@
-namespace FusionExamples.Tanknarok
-{
-	public class TargetMarker : AutoReleasedFx
-	{
-		protected override float Duration => 2.0f;
-	}
-}
